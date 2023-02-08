d5951718763d0c759f0e4ede3d972ff977d61096
ARBUCHELI <radio_seattle@hotmail.com>
2023-02-08 17:24:18 -0500

    adding data

---
+++ hacker-new-trends.sql
@@ -0,0 +1,157 @@
+/*CREATE TABLE hacker_news (
+    title TEXT,
+    user TEXT,
+    score INTEGER,
+    timestamp DATETIME,
+    url TEXT
+)*/
+
+/*INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES(NULL, 'coldtea', NULL, '27-jan-2014', NULL);
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES(NULL, 'etanol', NULL, '23-oct-2011', NULL);
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES(NULL, NULL, NULL, '28-feb-2016', NULL);
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES(NULL, 'Locke1689', NULL, '12-aug-2014', NULL);
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES(NULL, 'miloshadzic', NULL, '06-mar-2013', NULL);
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES(NULL, 'salsakran', NULL, '14-apr-2011', NULL);*/
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES('#McConnelling', 'deepblueocean', 2, '18-mar-2014', 'http://www.mcconnelling.org/');
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES('A floating self-sustaining home that would respond to rising sea levels', 'tudorw', 1, '19-nov-2012', 'http://www.kickstarter.com/projects/whim/recycled-island');
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES('What Ever Happened to Facebook''s Rooms?', 'mirandak4', 2, '04-nov-2016', 'https://backchannel.com/when-facebook-cleared-out-thousands-of-rooms-ee42a4154b33#.hpgimx9i1');
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES(NULL, 'paulsutter', NULL, '02-jul-2016', NULL);
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES(NULL, 'sitkack', NULL, '28-apr-2014', NULL);
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES(NULL, 'cjsthompson', NULL, '28-nov-2015', NULL);
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES(NULL, 'evgen', NULL, '21-05-2017', NULL);
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES(NULL, 'adventured', NULL, '07-feb-2013', NULL);
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES(NULL, 'jhanschoo', NULL, '26-jan-2016', NULL);
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES(NULL, 'greggman', NULL, '30-oct-2015', NULL);
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES(NULL, NULL, NULL, '17-06-2016', NULL);
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES('Epic Privacy Browser a more secure and private chromium-based web browser', 'sinak', 1, '24-feb-2015', 'https://www.epicbrowser.com');
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES(NULL, 'lobster_johnson', NULL, '17-dec-2016', NULL);
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES(NULL, 'shrimp_emoji', NULL, '20-apr-2017', NULL);
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES('Ireland plans to make high-speed broadband a right for every citizen', 'ohjeez', 2, '04-jun-2016', 'http://qz.com/699067/ireland-plans-to-make-high-speed-broadband-a-right-for-every-citizen/');
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES(NULL, 'cturner', '08-may-2010', NULL);
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES('Lessons in i18n', 'quile', 2, '16-dec-2010', NULL);
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES(,NULL);
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES(,NULL);
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES(,NULL);
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES(,NULL);
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES(,NULL);
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES(,NULL);
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES(,NULL);
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES(,NULL);
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES(,NULL);
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES(,NULL);
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES();
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES();
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES();
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES();
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES();
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES();
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES();
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES();
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES();
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES();
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES();
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES();
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES();
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES();
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES();
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES();
+
+INSERT INTO hacker_news(title, user, score, timestamp, url)
+VALUES();
